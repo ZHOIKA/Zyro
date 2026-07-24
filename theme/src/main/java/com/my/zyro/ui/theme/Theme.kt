ea6b4fa1505a036689d027ec846a64d039bfefa8
unknown <andradeenzo16@outlook.com>
2026-07-24 16:05:35 -0300

    fix(theme): add surfaceTint param to ColorScheme constructor

--- theme/src/main/java/com/my/zyro/ui/theme/Theme.kt
+++ theme/src/main/java/com/my/zyro/ui/theme/Theme.kt
@@ -18,7 +18,6 @@ import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -81,6 +80,7 @@ fun getZyroColorScheme(
         inverseSurface = ZyroGray100,
         inverseOnSurface = ZyroBlack,
         inversePrimary = ZyroVioletLight,
+        surfaceTint = surface,
     )
 }
 
